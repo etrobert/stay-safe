d56b845e9164f9f40c93c6ba4b315dd9f1d28576
erobert <etiennerobert33@gmail.com>
2023-04-22 12:42:48 +0200

    Add mapbox map

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
+'use client';
+import { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
 import styles from './page.module.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default function Home() {
+  useEffect(() => {
+    // TO MAKE THE MAP APPEAR YOU MUST
+    // ADD YOUR ACCESS TOKEN FROM
+    // https://account.mapbox.com
+    mapboxgl.accessToken =
+      'pk.eyJ1IjoiZXRyb2JlcnQiLCJhIjoiY2xncnRyYml5MG1xODNmb2g0eHp0ZjVnbSJ9.HQ15TYvSzgdrUWLZxjcFdg';
+    const map = new mapboxgl.Map({
+      container: styles.map, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  });
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>Hello World!</div>
+      <div id={styles.map} />
     </main>
   );
 }
