72f7e68d91a4edede7989b974454a4949450c90a
erobert <etiennerobert33@gmail.com>
2023-04-22 16:54:40 +0200

    Add a secondary positive heatmap

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,59 @@ export default function Home() {
         type: 'geojson',
         data: 'earthquakes.geojson',
       });
-
+      map.addLayer(
+        {
+          id: 'earthquakes-heat-2',
+          type: 'heatmap',
+          source: 'earthquakes',
+          paint: {
+            // Increase the heatmap weight based on frequency and property magnitude
+            'heatmap-weight': [
+              'interpolate',
+              ['linear'],
+              ['get', 'mag'],
+              -6,
+              1,
+              0,
+              0,
+            ],
+            // Increase the heatmap color weight weight by zoom level
+            // heatmap-intensity is a multiplier on top of heatmap-weight
+            'heatmap-intensity': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              0,
+              1,
+              9,
+              3,
+            ],
+            // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+            // Begin color ramp at 0-stop with a 0-transparancy color
+            // to create a blur-like effect.
+            'heatmap-color': [
+              'interpolate',
+              ['linear'],
+              ['heatmap-density'],
+              0,
+              'transparent',
+              1,
+              'green',
+            ],
+            // Adjust the heatmap radius by zoom level
+            'heatmap-radius': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              0,
+              2,
+              9,
+              20,
+            ],
+          },
+        },
+        'waterway-label'
+      );
       map.addLayer(
         {
           id: 'earthquakes-heat',
